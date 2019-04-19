13cf8e6bb34b254d3a1b222dd71dc59ac7d47ee0
HIRONORI KIKUCHI <hiro.kikuchi.universe@gmail.com>
2019-04-20 00:00:25 +0700

    restructure project folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 
 function App() {
   return (
